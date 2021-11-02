145c16c893de35e5934579849f05d2736f3b346c
Jhon Erlind Davila Mendez <jhondavilamendez@gmail.com>
2021-11-02 00:04:45 -0500

    commit

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ import Model from './Model';
 import { modelConfig } from './Config';
 import { connectToStore } from "./StoreConnect"
 
+const createModel = (opts) => {
+    return new Model(opts);
+}
+
 const WTools = {
     Request,
     Store,
     Model,
     connectToStore,
-    modelConfig
+    modelConfig,
+    createModel
 }
-
 export default WTools;
+
+
+
 
 export {
     Request,
     Store,
     Model,
     connectToStore,
-    modelConfig
+    modelConfig,
+    createModel
 };
